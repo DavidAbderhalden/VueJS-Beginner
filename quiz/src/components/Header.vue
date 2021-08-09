361ef6e311bbd7a9ed4f081d4d5e78b008dd2c5a
David Abderhalden <david.abderhalden@swisscom.com>
2021-08-09 16:42:25 +0200

    Page Setup
    Setting up the main two components as well as their CSS

--- quiz/src/components/Header.vue
+++ quiz/src/components/Header.vue
@@ -1,6 +1,23 @@
 <template class="header">
-    <nav class="nav">
-        <a class="nav-link active" aria-current="page" href="#">Quiz</a>
-        <a class="nav-link" href="#">Correct: 5/5</a>
+    <nav class="header__nav">
+        <h2 class="header__nav__title" >Quiz</h2>
+        <p class="header__nav__score" >Correct: 5/5</p>
     </nav>
 </template>
+
+
+<style>
+.header__nav {
+    width: 60%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid gray;
+
+}
+.header__nav__score{
+    margin-left: 50%;
+    padding: 0px;
+
+}
+</style>
--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -1,3 +1,75 @@
 <template class="qbox">
-    <h2 class="qbox__title">QuestionBox</h2>
+  <div class="qbox__wrapper">
+    <h2 class="qbox__wrapper__title">QuestionBox</h2>
+    <table class="qbox__wrapper__table">
+      <tr>
+        <th class="qbox__wrapper__table__question">Question</th>
+      </tr>
+      <tr>
+        <td class="qbox__wrapper__table__answer">Answer 1</td>
+      </tr>
+      <tr>
+        <td class="qbox__wrapper__table__answer">Answer 2</td>
+      </tr>
+      <tr>
+        <td class="qbox__wrapper__table__answer">Answer 3</td>
+      </tr>
+      <tr>
+        <td class="qbox__wrapper__table__answer">Answer 4</td>
+      </tr>
+    </table>
+
+    <div class="qbox__wrapper__footer">
+      <button class="qbox__wrapper__footer__button">Submit</button>
+      <button class="qbox__wrapper__footer__button">Next</button>
+    </div>
+  </div>
 </template>
+
+
+<style>
+.qbox__wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  margin-top: 35px;
+  background-color: rgb(238, 238, 238);
+  padding-bottom: 50px;
+}
+
+
+.qbox__wrapper__table {
+  width: 50%;
+  border-spacing: 20px;
+  margin-bottom: 20px;
+}
+
+.qbox__wrapper__table__question {
+  padding: 10px;
+  font-size: 20px;
+  border-bottom: 1px solid black;
+}
+
+.qbox__wrapper__table__answer {
+  padding: 8px;
+}
+
+
+.qbox__wrapper__footer{
+    display: flex;
+    justify-content: space-between;
+    width: 25%;
+}
+
+.qbox__wrapper__footer__button{
+    height: 30px;
+    width: 90px;
+    background-color: rgba(255, 181, 112, 0.986);
+    border: 1px solid rgb(190, 190, 190);
+}
+
+.qbox__wrapper__footer__button:hover{
+    background-color: rgba(255, 157, 65, 0.986);
+}
+</style>
